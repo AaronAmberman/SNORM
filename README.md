b2478c2b7f6dc87912e6dbaf2b008b7d45bb010f
Aaron Amberman <aaronamberman@gmail.com>
2020-12-24 20:09:54 -0500

    Initial check-in

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SNORM
+Simple .NET Object Relational Mapper.
+
+# MS SQL Only
+This SQL API only works with Mircosoft SQL Server as it requires the use of TVP's.
---
+++ SNORM/SNORM/SimpleSqlService.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace SNORM
+{
+    /// <summary>A simple SQL class that provides ExecuteNonQuery (for non-SELECT statements) and ExecuteQuery (for SELECT statements).</summary>
+    public static class SimpleSqlService
+    {
+        /// <summary>Gets or sets whether or not to automatically connect and disconnect on each method call. Default is true.</summary>
+        public static bool AutoConnect { get; set; } = true;
+
+        /// <summary>Gets or sets the method to call for logging (only errors are logged).</summary>
+        public static Action<string> Log { get; set; } = DefaultLogging;
+
+        /// <summary>Gets or sets the SQL connection.</summary>
+        public static SqlConnection Connection { get; set; }
+
+        private static void DefaultLogging(string entry)
+        {
+            Debug.WriteLine(entry);
+        }
+
+        /// <summary>Executes a Transact-SQL statement against the connection and returns the number of rows affected or -1 if an error occured.</summary>
+        /// <param name="query">The Transact-SQL statement to execute.</param>
+        /// <param name="commandType">The type of command.</param>
+        /// <param name="parameters">Parameters, if any, for the Transact-SQL statement.</param>
+        /// <returns>The number of rows affected or -1 if an error occured.</returns>
+        public static int ExecuteNonQuery(string query, CommandType commandType, params SqlParameter[] parameters)
+        {
+            int returnValue = -1;
+
+            SqlTransaction transaction = null;
+
+            try
+            {
+                if (Connection == null)
+                {
+                    Log("The connection is null and this cannot be, please set the Connection property.");
+
+                    return returnValue;
+                }
+
+                if (Connection.State == ConnectionState.Connecting || Connection.State == ConnectionState.Executing || Connection.State == ConnectionState.Fetching)
+                {
+                    Log("The connection is currently busy either connecting, executing or fetching. Failure.");
+
+                    return returnValue;
+                }
+
+                if (AutoConnect)
+                {
+                    if (Connection.State == ConnectionState.Closed)
+                        Connection.Open();
+                }
+
+                transaction = Connection.BeginTransaction();
+
+                SqlCommand command = new SqlCommand(query, Connection, transaction)
+                {
+                    CommandType = commandType
+                };
+
+                if (parameters.Length > 0)
+                    command.Parameters.AddRange(parameters);
+
+                returnValue = command.ExecuteNonQuery();
+
+                transaction.Commit();
+            }
+            catch(Exception ex)
+            {
+                transaction?.Rollback();
+
+                Log($"An error occurred attempting to run ExecuteNonQuery...{Environment.NewLine}{ex}");
+            }
+
+            transaction?.Dispose();
+
+            if (AutoConnect)
+                Connection.Close();
+
+            return returnValue;
+        }
+
+        /// <summary>Executes a Transact-SQL statement against the connection and returns the results or null if an error occured.</summary>
+        /// <param name="query">The Transact-SQL statement to execute.</param>
+        /// <param name="commandType">The type of command.</param>
+        /// <param name="parameters">Parameters, if any, for the Transact-SQL statement.</param>
+        /// <returns>The results or null if an error occured.</returns>
+        public static object[][] ExecuteQuery(string query, CommandType commandType, params SqlParameter[] parameters)
+        {
+            return null;
+        }
+    }
+}
