552c211676dd213d97f164c91c8688c1fcc6de8a
Aaron Amberman <aaronamberman@gmail.com>
2021-01-02 21:51:36 -0500

    Added attribute classes

---
+++ SNORM/SNORM/ORM/SqlColumnAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SNORM.ORM
+{
+    /// <summary>Describes a SQL column name alias so that the property name does not have to be used in the ORM.</summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class SqlColumnAttribute : Attribute
+    {
+        /// <summary>Gets the column name alias.</summary>
+        public string ColumnName { get; }
+
+        /// <summary>Initializes a new instance of <see cref="SqlColumnAttribute"/>.</summary>
+        /// <param name="columnName">The name of the column to reference in the table.</param>
+        public SqlColumnAttribute(string columnName)
+        {
+            ColumnName = columnName;
+        }
+    }
+}
---
+++ SNORM/SNORM/ORM/SqlTableAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SNORM.ORM
+{
+    /// <summary>Describes a SQL table name alias so that the type name does not have to be used in the ORM.</summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class SqlTableAttribute : Attribute
+    {
+        /// <summary>Gets the table name alias.</summary>
+        public string TableName { get;}
+
+        /// <summary>Initializes a new instance of <see cref="SqlTableAttribute"/>.</summary>
+        /// <param name="tableName">The name of the table to reference in the database.</param>
+        public SqlTableAttribute(string tableName)
+        {
+            TableName = tableName;
+        }
+    }
+}
